707d506dbc6f146795c4af5be9021967bcce09d9
HL <hoanglinh171038@gmail.com>
2023-07-26 13:57:19 +0700

    11.2 style for form/input

--- app/components/inputs/CountrySelect.tsx
+++ app/components/inputs/CountrySelect.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import useCountries from '@/app/hooks/useCountries';
+import Image from 'next/image';
 import Select from 'react-select';
 
 export type CountrySelectValue = {
@@ -23,9 +24,31 @@ const CountrySelect:React.FC<CountrySelectProps> =({
     const {getAll} = useCountries()
     return (
         <Select
+        placeholder="Any where"
         value={value}
         onChange={(value)=>onChange(value as CountrySelectValue)}
         options={getAll()}
+        isClearable
+        formatOptionLabel={(option:any)=>(
+            <div
+                className='
+                    flex
+                    items-center
+                    gap-3
+                '
+            >
+                <Image 
+                    src={`https://flagcdn.com/${option.value.toLowerCase()+'.svg'}`}
+                    width={30} 
+                    height={30}
+                    alt="Sounth Africa" 
+                />
+                <div>
+                    {option.label}
+                    <span className='text-neutral-800 ml-1'>{option.region}</span>
+                </div>
+            </div>
+        )}
       />
     )
 }
