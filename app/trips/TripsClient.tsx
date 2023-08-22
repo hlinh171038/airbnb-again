f03c1f50f2b7af60c2617ea6945b58a8d0f27022
HL <hoanglinh171038@gmail.com>
2023-08-22 13:42:15 +0700

    47.finish trips page

--- app/trips/TripsClient.tsx
+++ app/trips/TripsClient.tsx
@@ -10,6 +10,8 @@ import TripsSearch from "../components/trips/TripsSearch"
 import { SafeUser, safeReservation } from "../types"
 import {useCallback, useMemo, useState} from 'react'
 import useCountries from "../hooks/useCountries"
+import { Pagination, Stack } from "@mui/material"
+import Footer from "../components/Footer"
 
 interface TripsClientProps {
     currentUser: SafeUser | null
@@ -26,6 +28,8 @@ const TripsClient:React.FC<TripsClientProps> = ({
     const [uppdateRe,setUppdateRe] = useState(reservations)
     const [toggle,setToggle] = useState(false);
     const {getByValue} = useCountries();
+    const [currentPage, setCurrentPage] = useState(1);
+    const [countPerPages,setCountPerPage] = useState(3);
 
 
     const handleSelect = useCallback((item:string)=>{
@@ -70,57 +74,79 @@ const TripsClient:React.FC<TripsClientProps> = ({
         result = reservations.filter((item)=>item.listing.locationValue as any  === value )
         setUppdateRe(result);
     },[reservations])
+
+    // pagination
+    const start = currentPage * countPerPages -countPerPages;
+    const end = countPerPages * currentPage;
+
+    const pagin = [];
+    for(let i=0;i<Math.ceil(uppdateRe.length/countPerPages);i++)
+    {
+        pagin.push(i)
+    }
+    // handle change pagination
+    const handlePagination = useCallback((e:any,p: any)=>{
+        setCurrentPage(p)
+    },[currentPage])
     return (
-       <Container>
-        <Tag
-            tag1="Trip Listing"
-        />
-        <Header 
-            title="Danh sách chuyến đi "
-            subtitle="Nơi bạn đang đến, nơi bạn đang đi "
-            big
-        />
-        <div
-            className="
-                grid
-                grid-cols-2
-                mt-8
-            "
-            style={{gridTemplateColumns: "auto auto auto auto"}}
-        >
-            <div>
-                <TripsSearch 
-                    reservations ={reservations}
-                    handleValue = {handleValue}
-                />
-            </div>
-            <div style={{gridArea:"1/ 2 / span 2 / span 3"}}>
-                <div className="flex justify-end px-4  relative py-4 transition-all cursor-pointer">
-                   
-                       <div className=" absolute top-0 right-0 border-b-[1px] border-rose-500 w-[200px] capitalize hover:shadow-lg" onClick={handleToggleSelect}>{selectValue}</div>
-                       <BiDownArrow className="absolute top-[0.5rem] right-0" size={10} onClick={handleToggleSelect}/>
-                            <div className={`absolute top-7 right-0 bg-neutral-100 w-[200px] px-2 py-2 ${toggle ?"block": "hidden"}`} >
-                                {select.map((item: string) =>{
-                                    return <div onClick={()=>handleSelect(item)} className={`capitalize hover:bg-neutral-200    `}>{item}</div>
-                                })}
-                            </div>
-                       </div>
-                <div
-                    className="
-                    "
-                >
-                    {uppdateRe.map((reservation) =>{
-                        return <TripsCard 
-                                    key={reservation.id}
-                                    currentUser={currentUser}
-                                    data ={reservation.listing}
-                                    reservation ={reservation}
-                                 />
-                    })}
+       <div>
+        <Container>
+            <Tag
+                tag1="Trip Listing"
+            />
+            <Header 
+                title="Danh sách chuyến đi "
+                subtitle="Nơi bạn đang đến, nơi bạn đang đi "
+                big
+                center
+            />
+            <div
+                className="
+                    grid
+                    grid-cols-2
+                    mt-8
+                "
+                style={{gridTemplateColumns: "auto auto auto auto"}}
+            >
+                <div>
+                    <TripsSearch 
+                        reservations ={reservations}
+                        handleValue = {handleValue}
+                    />
+                </div>
+                <div style={{gridArea:"1/ 2 / span 2 / span 3"}}>
+                    <div className="flex justify-end px-4  relative py-4 transition-all cursor-pointer">
+                    
+                        <div className=" absolute top-0 right-0 border-b-[1px] border-rose-500 w-[200px] capitalize hover:shadow-lg" onClick={handleToggleSelect}>{selectValue}</div>
+                        <BiDownArrow className="absolute top-[0.5rem] right-0" size={10} onClick={handleToggleSelect}/>
+                                <div className={`absolute top-7 right-0 bg-neutral-100 w-[200px] px-2 py-2 ${toggle ?"block": "hidden"}`} >
+                                    {select.map((item: string) =>{
+                                        return <div onClick={()=>handleSelect(item)} className={`capitalize hover:bg-neutral-200    `}>{item}</div>
+                                    })}
+                                </div>
+                        </div>
+                    <div
+                        className="
+                        "
+                    >
+                        {uppdateRe.slice(start,end).map((reservation) =>{
+                            return <TripsCard 
+                                        key={reservation.id}
+                                        currentUser={currentUser}
+                                        data ={reservation.listing}
+                                        reservation ={reservation}
+                                    />
+                        })}
+                    </div>
                 </div>
             </div>
-        </div>
-       </Container>
+            {/* pagination */}
+            <Stack spacing={2} className="mt-3 mb-3 flex justify-end">
+                <Pagination count={pagin.length} variant="outlined" shape="rounded" className="flex justify-end" onChange={handlePagination}/>
+            </Stack>
+        </Container>
+        <Footer />
+       </div>
     )
 }
 
